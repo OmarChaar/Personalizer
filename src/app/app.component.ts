8fa0105c38dc046cb70a6af3ec8b65cd907aba76
Omar Chaar <oishaar@gmail.com>
2023-03-11 23:12:23 -0300

    dynamic favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { SessionStorageService } from './services/sessionStorage/session-storage.service';
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { animation } from './animations';
 import { FirebaseService } from './services/firebase/firebase.service';
 import { ConstantsService } from './services/constants/constants.service';
@@ -22,11 +22,23 @@ export class AppComponent {
     private router: Router,
     private firebaseService: FirebaseService,
     private sessionStorageService: SessionStorageService,
-    public constantsService: ConstantsService
+    public constantsService: ConstantsService,
+    private renderer: Renderer2
   ) {}
 
 
   async ngOnInit() {
+
+    const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    console.log("isDarkMode", isDarkMode);
+    const faviconPath = isDarkMode ? '/assets/favicon-dark.ico' : '/assets/favicon-light.ico';
+    const favicon = this.renderer.createElement('link');
+    favicon.setAttribute('rel', 'icon');
+    favicon.setAttribute('type', 'image/x-icon');
+    favicon.setAttribute('href', faviconPath);
+    this.renderer.appendChild(document.head, favicon);
+
+
     const clientCredentials = this.sessionStorageService.get('clientCredentials') || undefined;
     if(clientCredentials) {
       await this.firebaseService.login(clientCredentials.id, clientCredentials.apartment);
